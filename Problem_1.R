6d41867fd3a4eb96b344f073457b6de4f9b33eaf
Alexander Parvanov <carnge@abv.bg>
2018-03-22 13:10:33 +0200

    Homework 1 , Problems 1 & 2

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,32 @@
+# Homework 1 
+# Problem 1
 
+#a) 
+x <- c(4,1,1,4)
+# b) 
+
+y <- c(1,4)
+
+# c) The result is the following as we have 2 vectors of different size. The smaller vector is recycled and becomes c(1,4,1,4) 
+
+x-y
+
+# d) 
+s <- c(x,y)
+
+# e)
+rep(s,10)
+srep <- rep(s,10)
+length(srep)
+
+# f)
+rep(s, each=3)
+
+# g)
+
+seq(7,21, by=1)
+7:21
+
+# h)
+lseq <- seq(7,21, by=1)
+length(lseq)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,44 @@
+# Homework 1
+# Problem 2
+
+# a)
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+# b)
+dff <-  xmax - xmin
+
+# c)
+avgMAX <- mean(xmax)
+avgMIN <- mean(xmin)
+
+# d)
+xmin [xmin < avgMIN]
+
+# e)
+xmin [xmax > avgMAX]
+
+# f)
+names(xmin) <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri' , 'Sat' , 'Sun')
+names(xmax) <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri' , 'Sat' , 'Sun')
+
+# g)
+temperature <- data.frame(xmin,xmax)
+
+# h)
+temperature <- within(temperature,{xminFahrenheit <- xmin*9/5 + 32})
+
+#i)
+ftemp <- data.frame(
+  Max_temp_Fahr = xmax*9/5 + 32 , 
+  Min_temp_Fahr = xmin*9/5 + 32)
+
+# j)
+
+FivedaytempFahrenheit  <- data.frame(
+  Max_temp_Fahr = xmax[seq(1:5)]*9/5+32,
+  Min_temp_Fahr = xmin[seq(1:5)]*9/5+32)
+
+FivedaytempFahrenheit <- data.frame(
+  Max_temp_Fahr = xmax[seq(length(xmax)-2)]*9/5+32 ,
+  Min_temp_Fahr = xmin[seq(length(xmin)-2)]*9/5+32)
 
